c4130dcfc2b141ad382ecf25d5c86ab1d32086b5
alpikohen <alpayboyan@gmail.com>
2026-05-24 12:55:59 +0300

    requests library is replaced with cloudscraper library to bypass Cloudflare protection on Exo. Blizzard and Apple tracking is also added.

--- exophase.py
+++ exophase.py
@@ -1,19 +1,20 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 from openpyxl import Workbook, load_workbook
 
 url_general = "https://www.exophase.com/leaderboard/"
-response = requests.get(url_general)
-general_data = response.content
+scraper = cloudscraper.create_scraper()
+
+general_data = scraper.get(url_general).text
 soup = BeautifulSoup(general_data, "html.parser")
 general_user_number = soup.find_all("span", {"class":"big_number"})
+print(general_user_number)
 message = str(general_user_number[1]).split('>')
 message2 = message[1].split('<')
 general_total = int(message2[0].replace(',', ''))
 
 url_psn = "https://www.exophase.com/psn/leaderboard/"
-response = requests.get(url_psn)
-psn_data = response.content
+psn_data = scraper.get(url_psn).text
 soup = BeautifulSoup(psn_data, "html.parser")
 psn_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(psn_user_number[1]).split('>')
@@ -21,8 +22,7 @@ message2 = message[1].split('<')
 psn_total = int(message2[0].replace(',', ''))
 
 url_xbox = "https://www.exophase.com/xbox/leaderboard/"
-response = requests.get(url_xbox)
-xbox_data = response.content
+xbox_data = scraper.get(url_xbox).text
 soup = BeautifulSoup(xbox_data, "html.parser")
 xbox_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(xbox_user_number[1]).split('>')
@@ -30,8 +30,7 @@ message2 = message[1].split('<')
 xbox_total = int(message2[0].replace(',', ''))
 
 url_steam = "https://www.exophase.com/steam/leaderboard/"
-response = requests.get(url_steam)
-steam_data = response.content
+steam_data = scraper.get(url_steam).text
 soup = BeautifulSoup(steam_data, "html.parser")
 steam_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(steam_user_number[1]).split('>')
@@ -39,17 +38,23 @@ message2 = message[1].split('<')
 steam_total = int(message2[0].replace(',', ''))
 
 url_ea = "https://www.exophase.com/origin/leaderboard/"
-response = requests.get(url_ea)
-ea_data = response.content
+ea_data = scraper.get(url_ea).text
 soup = BeautifulSoup(ea_data, "html.parser")
 ea_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(ea_user_number[1]).split('>')
 message2 = message[1].split('<')
 ea_total = int(message2[0].replace(',', ''))
 
+url_blizzard = "https://www.exophase.com/blizzard/leaderboard/"
+blizzard_data = scraper.get(url_blizzard).text
+soup = BeautifulSoup(blizzard_data, "html.parser")
+blizzard_user_number = soup.find_all("span", {"class":"big_number"})
+message = str(blizzard_user_number[1]).split('>')
+message2 = message[1].split('<')
+blizzard_total = int(message2[0].replace(',', ''))
+
 url_retro = "https://www.exophase.com/retro/leaderboard/"
-response = requests.get(url_retro)
-retro_data = response.content
+retro_data = scraper.get(url_retro).text
 soup = BeautifulSoup(retro_data, "html.parser")
 retro_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(retro_user_number[1]).split('>')
@@ -57,8 +62,7 @@ message2 = message[1].split('<')
 retro_total = int(message2[0].replace(',', ''))
 
 url_gplay = "https://www.exophase.com/android/leaderboard/"
-response = requests.get(url_gplay)
-gplay_data = response.content
+gplay_data = scraper.get(url_gplay).text
 soup = BeautifulSoup(gplay_data, "html.parser")
 gplay_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(gplay_user_number[1]).split('>')
@@ -66,8 +70,7 @@ message2 = message[1].split('<')
 gplay_total = int(message2[0].replace(',', ''))
 
 url_gog = "https://www.exophase.com/gog/leaderboard/"
-response = requests.get(url_gog)
-gog_data = response.content
+gog_data = scraper.get(url_gog).text
 soup = BeautifulSoup(gog_data, "html.parser")
 gog_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(gog_user_number[1]).split('>')
@@ -75,8 +78,7 @@ message2 = message[1].split('<')
 gog_total = int(message2[0].replace(',', ''))
 
 url_ubisoft = "https://www.exophase.com/uplay/leaderboard/"
-response = requests.get(url_ubisoft)
-ubisoft_data = response.content
+ubisoft_data = scraper.get(url_ubisoft).text
 soup = BeautifulSoup(ubisoft_data, "html.parser")
 ubisoft_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(ubisoft_user_number[1]).split('>')
@@ -84,30 +86,38 @@ message2 = message[1].split('<')
 ubisoft_total = int(message2[0].replace(',', ''))
 
 url_epic = "https://www.exophase.com/epic/leaderboard/"
-response = requests.get(url_epic)
-epic_data = response.content
+epic_data = scraper.get(url_epic).text
 soup = BeautifulSoup(epic_data, "html.parser")
 epic_user_number = soup.find_all("span", {"class":"big_number"})
 message = str(epic_user_number[1]).split('>')
 message2 = message[1].split('<')
 epic_total = int(message2[0].replace(',', ''))
 
+url_apple = "https://www.exophase.com/apple/leaderboard/"
+apple_data = scraper.get(url_apple).text
+soup = BeautifulSoup(apple_data, "html.parser")
+apple_user_number = soup.find_all("span", {"class":"big_number"})
+message = str(apple_user_number[1]).split('>')
+message2 = message[1].split('<')
+apple_total = int(message2[0].replace(',', ''))
+
 exophase_username = "alpikohen"
 psn_username      = "alpikohen"
 xbox_username     = "alpikohen"
 steam_username    = "alpikohen"
-ea_username       = "alpikohenn"
+ea_username       = "alpikohen"
+blizzard_username = "alpikohen-2359"
 retro_username    = "alpikohen"
 gplay_username    = "alpikohen"
 gog_username      = "alpikohen"
 ubisoft_username  = "alpikohen"
 epic_username     = "alpikohen"
+apple_username    = "alpikohen"
 
 exophase_url = "https://www.exophase.com/"
 
 url_user_general = exophase_url + "user/" + exophase_username
-response = requests.get(url_user_general)
-general_user_data = response.content
+general_user_data = scraper.get(url_user_general).text
 soup = BeautifulSoup(general_user_data, "html.parser")
 general_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(general_ranking).split('>')
@@ -115,8 +125,7 @@ message2 = message[3].split('<')
 general_mine = int(message2[0].replace(',', ''))
 
 url_user_psn = exophase_url + "psn/user/" + psn_username
-response = requests.get(url_user_psn)
-psn_user_data = response.content
+psn_user_data = scraper.get(url_user_psn).text
 soup = BeautifulSoup(psn_user_data, "html.parser")
 psn_ranking = soup.find_all("span", {"class":"global-ranking tippy mb-1"})
 message = str(psn_ranking).split('>')
@@ -124,8 +133,7 @@ message2 = message[3].split('<')
 psn_mine = int(message2[0].replace(',', ''))
 
 url_user_xbox = exophase_url + "xbox/user/" + xbox_username
-response = requests.get(url_user_xbox)
-xbox_user_data = response.content
+xbox_user_data = scraper.get(url_user_xbox).text
 soup = BeautifulSoup(xbox_user_data, "html.parser")
 xbox_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(xbox_ranking).split('>')
@@ -133,8 +141,7 @@ message2 = message[3].split('<')
 xbox_mine = int(message2[0].replace(',', ''))
 
 url_user_steam = exophase_url + "steam/user/" + steam_username
-response = requests.get(url_user_steam)
-steam_user_data = response.content
+steam_user_data = scraper.get(url_user_steam).text
 soup = BeautifulSoup(steam_user_data, "html.parser")
 steam_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(steam_ranking).split('>')
@@ -142,17 +149,23 @@ message2 = message[3].split('<')
 steam_mine = int(message2[0].replace(',', ''))
 
 url_user_ea = exophase_url + "origin/user/" + ea_username
-response = requests.get(url_user_ea)
-ea_user_data = response.content
+ea_user_data = scraper.get(url_user_ea).text
 soup = BeautifulSoup(ea_user_data, "html.parser")
 ea_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(ea_ranking).split('>')
 message2 = message[3].split('<')
 ea_mine = int(message2[0].replace(',', ''))
 
+url_user_blizzard = exophase_url + "blizzard/user/" + blizzard_username
+blizzard_user_data = scraper.get(url_user_blizzard).text
+soup = BeautifulSoup(blizzard_user_data, "html.parser")
+blizzard_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
+message = str(blizzard_ranking).split('>')
+message2 = message[3].split('<')
+blizzard_mine = int(message2[0].replace(',', ''))
+
 url_user_retro = exophase_url + "retro/user/" + retro_username
-response = requests.get(url_user_retro)
-retro_user_data = response.content
+retro_user_data = scraper.get(url_user_retro).text
 soup = BeautifulSoup(retro_user_data, "html.parser")
 retro_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(retro_ranking).split('>')
@@ -160,8 +173,7 @@ message2 = message[3].split('<')
 retro_mine = int(message2[0].replace(',', ''))
 
 url_user_gplay = exophase_url + "android/user/" + gplay_username
-response = requests.get(url_user_gplay)
-gplay_user_data = response.content
+gplay_user_data = scraper.get(url_user_gplay).text
 soup = BeautifulSoup(gplay_user_data, "html.parser")
 gplay_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(gplay_ranking).split('>')
@@ -169,8 +181,7 @@ message2 = message[3].split('<')
 gplay_mine = int(message2[0].replace(',', ''))
 
 url_user_gog = exophase_url + "gog/user/" + gog_username
-response = requests.get(url_user_gog)
-gog_user_data = response.content
+gog_user_data = scraper.get(url_user_gog).text
 soup = BeautifulSoup(gog_user_data, "html.parser")
 gog_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(gog_ranking).split('>')
@@ -178,8 +189,7 @@ message2 = message[3].split('<')
 gog_mine = int(message2[0].replace(',', ''))
 
 url_user_ubisoft = exophase_url + "uplay/user/" + ubisoft_username
-response = requests.get(url_user_ubisoft)
-ubisoft_user_data = response.content
+ubisoft_user_data = scraper.get(url_user_ubisoft).text
 soup = BeautifulSoup(ubisoft_user_data, "html.parser")
 ubisoft_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(ubisoft_ranking).split('>')
@@ -187,35 +197,46 @@ message2 = message[3].split('<')
 ubisoft_mine = int(message2[0].replace(',', ''))
 
 url_user_epic = exophase_url + "epic/user/" + epic_username
-response = requests.get(url_user_epic)
-epic_user_data = response.content
+epic_user_data = scraper.get(url_user_epic).text
 soup = BeautifulSoup(epic_user_data, "html.parser")
 epic_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
 message = str(epic_ranking).split('>')
 message2 = message[3].split('<')
 epic_mine = int(message2[0].replace(',', ''))
 
+url_user_apple = exophase_url + "apple/user/" + apple_username
+apple_user_data = scraper.get(url_user_apple).text
+soup = BeautifulSoup(apple_user_data, "html.parser")
+apple_ranking = soup.find_all("span", {"class":"global-ranking tippy"})
+message = str(apple_ranking).split('>')
+message2 = message[3].split('<')
+apple_mine = int(message2[0].replace(',', ''))
+
 wb = load_workbook("Achievement_Track.xlsx")
-ws = wb["Sayfa1"]
+ws = wb["General"]
 ws['C3']  = psn_mine
 ws['C4']  = xbox_mine
 ws['C5']  = steam_mine
 ws['C6']  = ea_mine
-ws['C7']  = retro_mine
-ws['C8']  = gplay_mine
-ws['C9']  = gog_mine
-ws['C10'] = ubisoft_mine
-ws['C11'] = epic_mine
-ws['C12'] = general_mine
+ws['C7']  = blizzard_mine
+ws['C8']  = retro_mine
+ws['C9']  = gplay_mine
+ws['C10'] = gog_mine
+ws['C11'] = ubisoft_mine
+ws['C12'] = epic_mine
+ws['C13'] = apple_mine
+ws['C14'] = general_mine
 
 ws['D3']  = psn_total
 ws['D4']  = xbox_total
 ws['D5']  = steam_total
 ws['D6']  = ea_total
-ws['D7']  = retro_total
-ws['D8']  = gplay_total
-ws['D9']  = gog_total
-ws['D10'] = ubisoft_total
-ws['D11'] = epic_total
-ws['D12'] = general_total
+ws['D7']  = blizzard_total
+ws['D8']  = retro_total
+ws['D9']  = gplay_total
+ws['D10'] = gog_total
+ws['D11'] = ubisoft_total
+ws['D12'] = epic_total
+ws['D13'] = apple_total
+ws['D14'] = general_total
 wb.save("Achievement_Track.xlsx")
